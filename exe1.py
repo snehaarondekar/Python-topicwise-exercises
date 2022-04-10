12f027546c02ac78810685640d3cae853233350a
snehaarondekar <sneha.arondekar25@gmail.com>
2022-04-10 12:57:27 +0530

    Introduction to Programming Exercise 1: Mailing Address

---
+++ exe1.py
@@ -0,0 +1,8 @@
+# Exercise 1: Mailing Address
+# (Solved—9 Lines)
+# Create a program that displays your name and complete mailing address formatted in
+# the manner that you would usually see it on the outside of an envelope. Your program
+# does not need to read any input from the user.
+
+print("Name : Sneha Arondekar")
+print("Bd de Parc, 77700 Coupvray, France")
