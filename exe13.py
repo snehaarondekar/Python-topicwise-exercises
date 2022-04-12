780e66c790ad4c9aec43c1d20761d119fddb3d26
snehaarondekar <sneha.arondekar25@gmail.com>
2022-04-12 11:28:19 +0530

    Introduction to Programming Exercise 13:Making Change

---
+++ exe13.py
@@ -0,0 +1,40 @@
+# Exercise 13:Making Change
+#
+# Consider the software that runs on a self-checkout machine. One task that it must be
+# able to perform is to determine how much change to provide when the shopper pays
+# for a purchase with cash.
+# Write a program that begins by reading a number of cents from the user as an
+# integer. Then your program should compute and display the denominations of the
+# coins that should be used to give that amount of change to the shopper. The change
+# should be given using as few coins as possible. Assume that the machine is loaded
+# with pennies, nickels, dimes, quarters, loonies and toonies.
+
+# A one dollar coin was introduced in Canada in 1987. It is referred to as a
+# loonie because one side of the coin has a loon (a type of bird) on it. The two
+# dollar coin, referred to as a toonie, was introduced 9 years later. It’s name is
+# derived from the combination of the number two and the name of the loonie.
+
+CENTS_PER_TOONIES = 200
+CENTS_PER_LOONIES = 100
+CENTS_PER_QUARTER = 25
+CENTS_PER_DIME = 10
+CENTS_PER_NICKEL = 5
+
+cents = int(input("Enter number of cents :"))
+c = cents
+toonies = cents // CENTS_PER_TOONIES
+cents = cents % CENTS_PER_TOONIES
+
+loonies = cents // CENTS_PER_LOONIES
+cents = cents % CENTS_PER_LOONIES
+
+quarter = cents // CENTS_PER_QUARTER
+cents = cents % CENTS_PER_QUARTER
+
+dime = cents // CENTS_PER_DIME
+cents = cents % CENTS_PER_DIME
+
+nickel = cents // CENTS_PER_NICKEL
+cents = cents %CENTS_PER_NICKEL
+
+print("equivalent change for", c, "cents= ",toonies, "toonies,", loonies, "loonies,",quarter, "quarter,",dime,"dime,",nickel,"nickel,",cents,"pennies")
